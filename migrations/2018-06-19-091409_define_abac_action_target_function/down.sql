996f13585c30f12748eeaf21507c55b6d0b352e5
Aleksey Ivanov <ialexxei@gmail.com>
2018-06-19 12:45:46 +0300

    Add functions migrations

---
+++ migrations/2018-06-19-091409_define_abac_action_target_function/down.sql
@@ -0,0 +1,1 @@
+drop function abac_action_target(_attrs abac_attribute[]);
---
+++ migrations/2018-06-19-091723_define_abac_object_list_functions/down.sql
@@ -0,0 +1,4 @@
+drop function abac_object_list_1(_attr abac_attribute, _offset integer, _limit integer);
+drop function abac_object_list_2(_attr1 abac_attribute, _attr2 abac_attribute, _offset integer, _limit integer);
+drop function abac_object_list_3(_attr1 abac_attribute, _attr2 abac_attribute, _attr3 abac_attribute, _offset integer, _limit integer);
+drop function abac_object_list(_attrs abac_attribute[], _offset integer, _limit integer);
