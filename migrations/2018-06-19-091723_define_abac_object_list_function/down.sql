e6f4e5d90f13dfc61efc81e6bd91c7d9cce7457a
Aleksey Ivanov <ialexxei@gmail.com>
2018-08-04 22:42:44 +0300

    Export only abac_object_list function

---
+++ migrations/2018-06-19-091723_define_abac_object_list_function/down.sql
@@ -0,0 +1,1 @@
+drop function abac_object_list(_attrs abac_attribute[], _offset integer, _limit integer);
--- tests/functions/lib.rs
+++ tests/functions/lib.rs
@@ -5,9 +5,6 @@ extern crate uuid;
 mod abac_action_target;
 mod abac_authorize;
 mod abac_object_list;
-mod abac_object_list_1;
-mod abac_object_list_2;
-mod abac_object_list_3;
 mod abac_object_target;
 mod abac_subject_target;
 
